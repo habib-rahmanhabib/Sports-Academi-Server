b5ab51b419bcbf3b291836bfe2a5bc4aa1eefb87
habib-rahmanhabib <mdhabiburrahman80680@gmail.com>
2023-06-10 14:39:41 +0600

    post mathod added

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
 
 
 app.use(cors())
 app.use(express.json())
 
+
+const jwtVerify = (req, res, next) => {
+  const authorization = req.headers.authorization
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unathorization access' })
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SCRECT, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unathorization access' })
+    }
+    req.decoded = decoded;
+    next()
+  })
+}
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -28,6 +46,13 @@ async function run() {
     const instractorCollection = client.db("SportsAcademi").collection("instractor")
     const classCollection = client.db("SportsAcademi").collection("class")
     const enrollCollection = client.db("SportsAcademi").collection("allEnroll")
+
+    app.post('/jwt', (req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SCRECT, {expiresIn: '1hr' })
+    res.send({token})
+    })
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
@@ -44,6 +69,24 @@ async function run() {
       const result = await classCollection.find().toArray()
       return res.send(result)
     })
+
+    app.get('/enroll', jwtVerify, async(req, res)=>{
+      const email = req.query.email;
+      // console.log(email)
+      if(!email){
+       return res.send([]);
+      }
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({error: True, message: 'porviden access'})
+      }
+    
+      const query = {email: email};
+      // console.log(query)
+      const result = await enrollCollection.find(query).toArray();
+      res.send(result)
+    })
+
 
     //  post mathod
     app.post('/all-enroll', async (req, res) => {
